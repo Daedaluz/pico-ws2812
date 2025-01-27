7ff8ee7b57295bc8f362d6a8b0dc90f883d2e517
Tobias Assarsson <tobias.assarsson@gmail.com>
2025-01-27 18:19:20 +0100

    [wip] wifi udp

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
+#include "config.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "pico/multicore.h"
 #include "hardware/pio.h"
 #include "ws2812.pio.h"
-#include "config.h"
 
 static uint8_t stripes_data[WS2812_PIXELS * 3];
 static uint8_t init_data[3*3] = {255, 0, 0, 0, 255, 0, 0, 0, 255};
@@ -19,22 +20,52 @@ static void write_ws2812(PIO pio, uint sm) {
 	}
 }
 
-int main() {
-	stdio_init_all();
+static void led_main() {
 	PIO pio;
 	uint sm;
 	uint offset;
-
 	bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_PIN, 1, true);
 	hard_assert(success);
 	ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, false);
-	
 	memcpy(stripes_data, init_data, sizeof(init_data));
 
 	while(true) {
 		write_ws2812(pio, sm);
 		sleep_ms(1000);
 	}
+
 	pio_remove_program_and_unclaim_sm(&ws2812_program, pio, sm, offset);
+}
+
+static void recv(void* _unused, struct udp_pcb* pcb, struct pbuf* pbuf, const ip_addr_t* src, uint16_t port) {
+	printf("recv from %s:%d\n", ipaddr_ntoa(src), port);
+}
+
+int main() {
+	stdio_init_all();
+	multicore_launch_core1(led_main);
+
+	while(cyw43_arch_init() != 0) {
+		printf("WiFi initialization failed\n");
+		sleep_ms(1000);
+	};
+	cyw43_arch_enable_sta_mode();
+	while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASS, CYW43_AUTH_WPA2_AES_PSK, 10000) != 0) {
+		printf("WiFi connection failed \"%s\"\n", WIFI_SSID);
+		sleep_ms(1000);
+	};
+
+	struct udp_pcb* udp = udp_new();
+	udp_bind(udp, IP_ANY_TYPE, UDP_PORT);
+
+	while(true) {
+		udp_recv(udp, recv, NULL);
+		// Blink with wifi-led
+		cyw43_arch_gpio_put(0, true); 
+		sleep_ms(1000);
+		cyw43_arch_gpio_put(0, false);
+		sleep_ms(1000);
+	}
+
 	return 0;
 }
